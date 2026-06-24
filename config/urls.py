c86090975cb62fda2a962a84f078af1f903eecd7
zhongtotoro <s1f102400055@inad.org>
2026-06-24 23:14:47 +0900

    トップページを追加

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views as todo_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', todo_views.index, name='index'),
 ]
