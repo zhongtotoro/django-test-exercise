95f9b49a1a02fab4fd9145c9fb00d0524870bb3a
zhongtotoro <s1f102400055@inad.org>
2026-06-24 23:25:32 +0900

    ソート機能を追加

--- todo/views.py
+++ todo/views.py
@@ -10,10 +10,15 @@ def index(request):
         task = Task(title=request.POST['title'],
                     due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
+
+    if request.GET.get('order') == 'due':
+        tasks = Task.objects.order_by('due_at')
+    else:
+        tasks = Task.objects.order_by('-posted_at')
         
-    tasks = Task.objects.all()
-    
     context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
+
+
