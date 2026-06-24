1bee796be841e1f227dcca2a029790c53cc2622f
zhongtotoro <s1f102400055@inad.org>
2026-06-24 23:21:21 +0900

    テストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
@@ -54,3 +54,31 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+
+
+# ファイルの一番上のほうにある import 文に「Client」を追加するか、新しく書きます
+from django.test import TestCase, Client 
+# （他のimport文はそのまま残しておいて大丈夫です！）
+
+# --- 中略（今までのテストコード） ---
+
+# ▼ ファイルの一番下に、新しく画面テスト用のクラスを追加！
+class TodoViewTestCase(TestCase):
+    # GETメソッド（画面を普通に開く処理）のテスト
+    def test_index_get(self):
+        # 画像のコードをここに書きます（インデントに気をつけて！）
+        client = Client()
+        response = client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']), 0)
+
+    def test_index_post(self):
+        client = Client()
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        response = client.post('/', data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']), 1)
