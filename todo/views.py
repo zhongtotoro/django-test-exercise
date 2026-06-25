d9eef931c9887284f36710345c19903bba3537e5
zhongtotoro <s1f102400055@inad.org>
2026-06-25 11:25:55 +0900

    4弾

--- todo/views.py
+++ todo/views.py
@@ -16,10 +16,8 @@ def index(request):
         tasks = Task.objects.order_by('due_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
-    
+
     context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
-    
-
